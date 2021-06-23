a909d47db8a08924c7d6cc6345dea15df9a993a7
vjcitn <stvjc@channing.harvard.edu>
2021-06-22 23:45:42 -0400

    update to support downloading of selections (metadata only)

--- ui.R
+++ ui.R
@@ -1,10 +1,14 @@
+library(shinytoastr)
 shinyUI(
     fluidPage(
-        titlePanel("Select rows in the Data Table"),
+        useToastr(),
+        titlePanel("Search for Annotation Hub resources"),
         sidebarLayout(
             sidebarPanel(
-                actionButton("btnSend", "Send"),
-                width = 1
+                helpText("Use search box in table to filter records.  Click on rows to select for downloading the metadata."),
+                textOutput("notes"),
+                downloadButton("btnSend", "Download metadata on selections"),
+                width = 3
             ),
             mainPanel(
                 DT::dataTableOutput('tbl')
